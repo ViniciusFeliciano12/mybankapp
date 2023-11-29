e9b656f2929aebe16b89915b7db855db613bd81a
ViniciusFeliciano12 <devilmynear1@gmail.com>
2023-11-29 18:33:28 -0300

    creating docker for api

--- BankAPI/Program.cs
+++ BankAPI/Program.cs
@@ -1,35 +1,23 @@
-
-using Microsoft.AspNetCore.Cors;
 using BankAPI.Data;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-   {
-       c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Bank API", Version = "v1" });
-   });
+ builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(p => p.AddPolicy("corspolicy", build => {
-    build.WithOrigins("http://localhost:5041").WithOrigins("https://localhost:7090").AllowAnyMethod().AllowAnyHeader();
-}));
+ builder.Services.AddCors(p => p.AddPolicy("corspolicy", build => {
+     build.WithOrigins("http://localhost:5041").WithOrigins("https://localhost:7090").AllowAnyMethod().AllowAnyHeader();
+ }));
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API V1");
-        c.RoutePrefix = string.Empty;
-    });
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
-
 
 
 app.UseCors("corspolicy");
